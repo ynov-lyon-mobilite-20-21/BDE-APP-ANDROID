3b10b41e4195e855b33b9e37706727b5252b009e
lea <lea.fraioli38@gmail.com>
2020-10-14 11:15:33 +0200

    start main activity

---
+++ app/src/main/java/com/example/ynov_lyon_bde/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ynov_lyon_bde
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
