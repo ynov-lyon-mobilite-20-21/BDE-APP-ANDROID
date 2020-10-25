c235b170c528f9d9f877a0444698134111dfec16
Luca <luca.sardellitti.1995@gmail.com>
2020-10-25 19:30:05 +0100

    Création de la bottomNavigationView; Ajout des images events, logo et account

--- app/src/main/java/com/example/ynov_lyon_bde/MainActivity.kt
+++ app/src/main/java/com/example/ynov_lyon_bde/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.ynov_lyon_bde
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val bottomNavigation  = findViewById<BottomNavigationView>(R.id.bottom_navigation_view) //Récupération de l'id bottom navigation view
+        val homeFragment= HomeFragment()
+        val eventsFragment = EventsFragment()
+        val accountFragment = AccountFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        //Changement de fragment aux cliques des items du menu
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> makeCurrentFragment(homeFragment)
+                R.id.events -> makeCurrentFragment(eventsFragment)
+                R.id.account -> makeCurrentFragment(accountFragment)
+            }
+            true
+        }
     }
+
+    //Fonction permettant de changer de fragment
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.wrapper, fragment)
+            commit()
+        }
 }
