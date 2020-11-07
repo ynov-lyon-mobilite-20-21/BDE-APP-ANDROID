faf7f5eb8be2fa7178e16355c1d380513859aa93
PierreGourgouillon <72550004+PierreGourgouillon@users.noreply.github.com>
2020-11-07 19:53:37 +0100

    Splashscreen with onBoarding is working Little problem with splashscreen

--- app/src/main/java/com/example/ynov_lyon_bde/MainActivity.kt
+++ app/src/main/java/com/example/ynov_lyon_bde/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.ynov_lyon_bde
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
     }
 }
