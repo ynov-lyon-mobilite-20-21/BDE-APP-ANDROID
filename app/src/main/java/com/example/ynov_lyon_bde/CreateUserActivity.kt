5b5f2ae52a588496f96734afa97e2f2d40079654
lef <lef-aron@protonmail.com>
2020-11-25 10:42:58 +0100

    template create use

--- app/src/main/java/com/example/ynov_lyon_bde/CreateUserActivity.kt
+++ app/src/main/java/com/example/ynov_lyon_bde/CreateUserActivity.kt
@@ -4,10 +4,10 @@ package com.example.ynov_lyon_bde
 import android.accounts.Account
 import android.accounts.AccountManager
 import android.annotation.SuppressLint
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -20,20 +20,66 @@ class CreateUserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_createuser)
 
+        //return previous activity
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        //Spinner to take class for a new user
+        val spinnerC: Spinner = findViewById(R.id.spinnerClasse)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.classe_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerC.adapter = adapter
+        }
+
+        //Spinner to take faculty for a new user
+        val spinnerF: Spinner = findViewById(R.id.spinnerFaculty)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.faculty_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerF.adapter = adapter
+        }
+
+        //Spinner to take statut for a new user
+        val spinnerS: Spinner = findViewById(R.id.spinnerStatut)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.statut_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerF.adapter = adapter
+        }
+
         buttonValidate.setOnClickListener {
             // Take informations User
             val email = editTextMail.text
             val psswd = editTextPassword.text
-            val phone = editTextPhone.text
-            val classe = editTextClass.text
-            val faculty = editTextFaculty.text
+            val classe = spinnerC.getSelectedItem().toString()
+            val faculty = spinnerF.getSelectedItem().toString()
+            val statut = spinnerS.getSelectedItem().toString()
 
-
-// Instantiate the RequestQueue.
+            /*******SEND REQUEST TO API AND TAKE RESPONSE***********/
+            // Instantiate the RequestQueue.
             val queue = Volley.newRequestQueue(this)
             //val url = "https://self-buy-api.herokuapp.com/api/users"
             val url = "https://www.google.fr/"
-// Request a string response from the provided URL.
+            // Request a string response from the provided URL.
             val stringRequest = StringRequest(
                 Request.Method.GET, url,
                 { response ->
@@ -42,15 +88,21 @@ class CreateUserActivity : AppCompatActivity() {
                 },
                 { println("That didn't work!") })
 
-// Add the request to the RequestQueue.
+            // Add the request to the RequestQueue.
             queue.add(stringRequest)
 
-            //account manager
-            val am: AccountManager = AccountManager.get(this) // "this" references the current Context
 
+
+            //init an account manager
+            val am: AccountManager = AccountManager.get(this) // "this" references the current Context
             val accounts: Array<out Account> = am.getAccountsByType("com.google")
+
         }
 
+        fun signUp(email: String?, password: String?): String? {
+            // TODO: if user don't exist, register new user on the server and return its auth token
+            return null
+        }
     }
 
 
