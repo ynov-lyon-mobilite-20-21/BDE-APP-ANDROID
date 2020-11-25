8da8327e49c43f2fff74d19d4ffa462a31a3a11e
LucaSardellitti <luca.sardellitti.1995@gmail.com>
2020-11-25 08:19:09 +0100

    Remise en forme de la BottomNavigationView

--- app/src/main/java/com/example/ynov_lyon_bde/MainActivity.kt
+++ app/src/main/java/com/example/ynov_lyon_bde/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ynov_lyon_bde
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -11,17 +12,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val bottomNavigation  = findViewById<BottomNavigationView>(R.id.bottom_navigation_view) //Récupération de l'id bottom navigation view
+        val bottomNavigation  = findViewById<BottomNavigationView>(R.id.bottom_navigation)//Récupération de l'id bottom navigation view
+        val homeIcon = findViewById<ImageView>(R.id.home) //Récupération de l'id du logo "Home"
+
+        //On assigne des variables aux Fragments créés
         val homeFragment= HomeFragment()
         val eventsFragment = EventsFragment()
         val accountFragment = AccountFragment()
 
+        //La page (Fragment) par défaut
         makeCurrentFragment(homeFragment)
 
-        //Changement de fragment aux cliques des items du menu
+        //Changement de page (Fragement) aux cliques sur le logo "Home"
+        homeIcon.setOnClickListener{
+            makeCurrentFragment(homeFragment)
+        }
+
+        //Changement de fragment aux cliques des items du menu et de l'image (logo Home)
         bottomNavigation.setOnNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.home -> makeCurrentFragment(homeFragment)
                 R.id.events -> makeCurrentFragment(eventsFragment)
                 R.id.account -> makeCurrentFragment(accountFragment)
             }
